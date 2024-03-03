1c02caeed60bf9c60b54be9e9ff9b05c0d8826b3
shahidx0x <dev.imshahid@gmail.com>
2024-03-04 01:44:20 +0600

    feat : cta added , custom css class  added for cta

--- src/components/cta/Cta.tsx
+++ src/components/cta/Cta.tsx
@@ -1,10 +1,24 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
+
 const Cta = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center mt-[50px] md:mt-[100px] ">
-        <div className="w-[1018px] border border-red-500 bg-url"></div>
+    <>
+      <div className="flex justify-center items-center mt-[50px] md:mt-[100px]">
+        <div className="w-[1150px] flex justify-end pr-32 items-center gap-8  bg-cta h-[40rem]">
+          <div className="center-vertical gap-2">
+            <h1 className="text-[30px] font-bold">The Ways We Work</h1>
+            <p className="text-[18px] ">
+              How has our relationship with work changed?
+            </p>
+            <Button className="bg-[#4262ff] flex gap-3 rounded-full p-8 px-26 py-4">
+              <span>View The Report</span>
+              <ArrowRight />
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
